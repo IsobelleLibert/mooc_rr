392e47906b964beb30647057e9277ba5080ed53b
Konrad Hinsen <konrad.hinsen@fastmail.net>
2019-08-23 17:49:41 +0200

    Traduction du tutoriel Docker a Guix

---
+++ module4/ressources/moocrr_guix_jupyter/guix-channels.scm
@@ -0,0 +1,5 @@
+(list (channel
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (commit
+          "44881cad93801de9462d469500d582af79b99959")))
---
+++ module4/ressources/moocrr_guix_jupyter/manifest.scm
@@ -0,0 +1,8 @@
+(specifications->manifest
+ '("jupyter"
+   "python-matplotlib"
+   "python-numpy"
+   "python-pandas"
+   "python-statsmodels"
+   "python-nbconvert"))
+
